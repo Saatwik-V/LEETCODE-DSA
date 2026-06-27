c24e637c52053459b688839acdce17316b2ea789
V.Saatwik <saatwik455@gmail.com>
2026-06-28 03:33:25 +0530

    cq 3 ll

--- linked-list/1290.convert-binary-number-in-a-linked-list-to-integer.c
+++ linked-list/1290.convert-binary-number-in-a-linked-list-to-integer.c
@@ -12,45 +12,25 @@
  *     struct ListNode *next;
  * };
  */
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
 //Linked List
 //convert-binary-number-in-a-linked-list-to-integer
 //Time Complexity: O(n) where n is the number of nodes in the linked list. We traverse the linked list once to convert the binary number to an integer.
-//Space Complexity: O(n) where n is the number of nodes in the linked list. We are using an array to store the binary digits, which requires additional space proportional to the number of nodes in the linked list.
-//Approach: We can traverse the linked list and store the binary digits in an array. We then iterate through the array in reverse order, multiplying each binary digit by the corresponding power of 2 and summing the results to get the final integer value. Finally, we return the computed integer value.
+//Space Complexity: O(1) since we are using only a constant amount of extra space to store the result and intermediate variables
+//Approach: We can traverse the linked list and for each node, we left shift the current result by 1 (equivalent to multiplying by 2) and then add the value of the current node (0 or 1) to the result. This effectively builds the integer value as we traverse the linked list. Finally, we return the computed integer value.
 #include <stdio.h>
-#include <stdlib.h>
 struct ListNode {
       int val;
       struct ListNode *next;
   };    
 int getDecimalValue(struct ListNode* head) {
-    if(head==NULL)
+    int res=0;
+    while(head!=NULL)
     {
-        return -1;
-    }
-    int* arr=(int*)malloc(31*sizeof(int));
-    int n=0;
-    struct ListNode *temp=head;
-    while(temp!=NULL)
-    {
-        arr[n]=temp->val;
-        n++;
-        temp=temp->next;
-    }
-    int res=0,k=0;
-    for(int i=n-1;i>=0;i--)
-    {
-        res+=arr[i]*pow(2,k);
-        k++;
+        res=res<<1|head->val;
+        head=head->next;
     }
     return res;
+    
 }
 // @lc code=end
 
