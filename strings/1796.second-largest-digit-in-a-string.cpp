ced7885ef9a35ece8b5b7883d133de8a7e7869ec
V.Saatwik <saatwik455@gmail.com>
2026-09-05 16:51:49 +0530

    1796

--- strings/1796.second-largest-digit-in-a-string.cpp
+++ strings/1796.second-largest-digit-in-a-string.cpp
@@ -10,8 +10,8 @@
 //Time Complexity: O(n) where n is the size of the string s. We traverse the string once, and for each character, we perform constant time operations, leading to O(n) time complexity.
 //Space Complexity: O(1) as we are using a constant amount of extra space to store the largest and second largest digits, regardless of the size of the input string.
 //Approach: We can traverse the string and keep track of the largest and second largest digits we encounter. We initialize two variables, largest and second, to INT_MIN. For each character in the string, we check if it is a digit (between '0' and '9'). If it is, we convert it to an integer and compare it with the largest and second largest digits. If the digit is greater than the largest digit, we update second to be the value of largest, and then update largest to be the value of the current digit. If the digit is greater than second but less than largest, we update second to be the value of the current digit. After traversing the string, if second is still INT_MIN, it means there was no second largest digit, and we return -1.
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h> 
+using namespace std;
 
 class Solution {
 public:
